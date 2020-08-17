f8f50f7df07897e5514c3b845bd57ca170693bbf
Yuntao Bai <ytbai@princeton.edu>
2020-08-17 13:37:07 -0700

    terminal() returns 1 is the state is terminal, and 0 otherwise
    Assume mini-batch input

--- data_factory/simulators.py
+++ data_factory/simulators.py
@@ -51,5 +51,8 @@ class QSimulator():
     max_action = torch.argmax(Q_values).item()
     return max_action
   
+  def terminal(self, S):
+    return torch.min(torch.sum(S[:,0,:,:], dim = (1,2)), torch.tensor(1).type(torch.cuda.FloatTensor))
+
   def get_dataloader(self, batch_size = 32):
     return torch.utils.data.DataLoader(self.dataset, batch_size = batch_size, shuffle = True)
