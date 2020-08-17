71804aee751b43c3318641fdef92030c372e8327
Yuntao Bai <ytbai@princeton.edu>
2020-08-17 16:16:55 -0700

    Set dataLoader drop_last = True so as to avoid batch size 1
    which can cause problems for batch norm

--- data_factory/simulators.py
+++ data_factory/simulators.py
@@ -55,4 +55,4 @@ class QSimulator():
     return torch.min(torch.sum(S[:,0,:,:], dim = (1,2)), torch.tensor(1).type(torch.cuda.FloatTensor))
 
   def get_dataloader(self, batch_size = 32):
-    return torch.utils.data.DataLoader(self.dataset, batch_size = batch_size, shuffle = True)
+    return torch.utils.data.DataLoader(self.dataset, batch_size = batch_size, shuffle = True, drop_last = True)
