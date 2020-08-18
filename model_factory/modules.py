7a88ba0a37b9a9bf91a5f6e7e0516dd50b5f78b1
Yuntao Bai <ytbai@princeton.edu>
2020-08-18 03:29:18 -0700

    Add modules for residual blocks and residual downsample

--- model_factory/modules.py
+++ model_factory/modules.py
@@ -11,3 +11,52 @@ class GlobalAvgPool2d(nn.Module):
 
   def forward(self, x):
     return torch.mean(x, dim=(2,3))
+
+
+class ResidualBlock(nn.Module):
+  def __init__(self, in_channels):
+    super().__init__()
+    self.in_channels = in_channels
+    self.out_channels = in_channels
+
+    self.pre_res = nn.Sequential(
+                  nn.Conv2d(self.in_channels, self.in_channels, kernel_size = 3, padding = 1),
+                  nn.BatchNorm2d(self.in_channels),
+                  nn.ReLU(),
+                  nn.Conv2d(self.in_channels, self.in_channels, kernel_size = 3, padding = 1),
+                  )
+    self.post_res = nn.Sequential(
+                  nn.BatchNorm2d(self.in_channels),
+                  nn.ReLU(),
+                  )
+
+  def forward(self, x):
+    output = self.pre_res(x)
+    output = output + x
+    output = self.post_res(x)
+    return output
+
+class ResidualDownsample(nn.Module):
+  def __init__(self, in_channels):
+    super().__init__()
+    self.in_channels = in_channels
+    self.out_channels = 2*in_channels
+
+    self.pre_res = nn.Sequential(
+                  nn.Conv2d(self.in_channels, self.out_channels, kernel_size = 3, stride = 2, padding = 1),
+                  )
+    self.res_reduce = nn.Sequential(
+                  nn.AvgPool2d(kernel_size = 2, stride = 2),
+                  nn.Conv2d(self.in_channels, self.out_channels, kernel_size = 1),
+                  )
+    self.post_res = nn.Sequential(
+                  nn.BatchNorm2d(self.out_channels),
+                  nn.ReLU(),
+                  )
+
+  def forward(self, x):
+    output = self.pre_res(x)
+    output = output + self.res_reduce(x)
+    output = self.post_res(output)
+    return output
+
