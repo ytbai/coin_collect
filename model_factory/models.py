74591ff4c055670a9ac3a98994edf77f1c9a7ae9
Yuntao Bai <ytbai@princeton.edu>
2020-08-18 16:23:48 -0700

    Two more models for Q function

--- model_factory/models.py
+++ model_factory/models.py
@@ -29,3 +29,58 @@ class QValue(nn.Module):
 
   def forward(self, x):
     return self.seq(x)
+
+
+class QValueWide(nn.Module):
+  def __init__(self):
+    super().__init__()
+    
+    self.seq = nn.Sequential(
+        nn.Conv2d(4, 16, kernel_size = 3, padding = 1),
+        nn.ReLU(),
+
+        ResidualBlock(16),
+        ResidualDownsample(16),
+        ResidualBlock(32),
+
+        GlobalAvgPool2d(),
+        nn.BatchNorm1d(32),
+        nn.ReLU(),
+
+        nn.Linear(32, 9),
+        Rescale(1/np.sqrt(32)),
+    )
+
+
+
+  def forward(self, x):
+    return self.seq(x)
+
+
+
+class QValueDeep(nn.Module):
+  def __init__(self):
+    super().__init__()
+    
+    self.seq = nn.Sequential(
+        nn.Conv2d(4, 8, kernel_size = 3, padding = 1),
+        nn.ReLU(),
+
+        ResidualBlock(8),
+        ResidualBlock(8),
+        ResidualDownsample(8),
+        ResidualBlock(16),
+        ResidualBlock(16),
+
+        GlobalAvgPool2d(),
+        nn.BatchNorm1d(16),
+        nn.ReLU(),
+
+        nn.Linear(16, 9),
+        Rescale(1/np.sqrt(32)),
+    )
+
+
+
+  def forward(self, x):
+    return self.seq(x)
