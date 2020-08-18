4950dc05d4db76c5e5a69d6111565a9d2c760052
Yuntao Bai <ytbai@princeton.edu>
2020-08-18 09:19:18 -0700

    Notation change
    Also remove terminal() from QSimulator() so instead output "term" from data generator

--- data_factory/simulators.py
+++ data_factory/simulators.py
@@ -11,7 +11,7 @@ class QSimulator():
     self.Q = Q
     self.dataset = None
     self.renew_dataset()
-    self.T = 64
+    self.T = 16
 
   def renew_dataset(self):
     if self.dataset is not None:
@@ -29,30 +29,30 @@ class QSimulator():
   def simulate_once(self, eps):
     game = Game(self.Nx, self.Ny)
     while game.t < self.T and not game.terminal():
-      S = game.get_state()
-      A = self.eval_eps_greedy_action(S, eps)
-      Sp, R = game.advance(A)
-      self.dataset.append(S, A, R, Sp)
+      state = game.get_state()
+      action = self.eval_eps_greedy_action(state, eps)
+      state_prime, reward = game.advance(action)
+      term = torch.tensor(int(game.terminal())).type(torch.cuda.FloatTensor)
+      t = torch.tensor(game.t).type(torch.cuda.FloatTensor)
+      self.dataset.append(state, action, reward, state_prime, term, t)
     return self
   
-  def eval_eps_greedy_action(self, S, eps):
+  def eval_eps_greedy_action(self, state, eps):
     self.Q.eval()
     e = np.random.uniform()
     if e > eps:
-      max_action = self.eval_max_action(S)
+      max_action = self.eval_max_action(state)
       return max_action
     else:
       explore_action = np.random.randint(9)
       return explore_action
 
-  def eval_max_action(self, S):
+  def eval_max_action(self, state):
     self.Q.eval()
-    Q_values = self.Q(S.view(1, 4, self.Nx, self.Ny))
+    S = state.view(1, 4, self.Nx, self.Ny)
+    Q_values = self.Q(S)
     max_action = torch.argmax(Q_values).item()
     return max_action
   
-  def terminal(self, S):
-    return 1-torch.min(torch.sum(S[:,0,:,:], dim = (1,2)), torch.tensor(1).type(torch.cuda.FloatTensor))
-
   def get_dataloader(self, batch_size = 32):
     return torch.utils.data.DataLoader(self.dataset, batch_size = batch_size, shuffle = True, drop_last = True)
