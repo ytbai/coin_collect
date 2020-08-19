752a5da78389e1d4c568a16e09794f223c6bd3d7
Yuntao Bai <ytbai@princeton.edu>
2020-08-19 04:33:35 -0700

    Move simulate_once() to parent Simulator() class
    For QSimualtor, eps is now an attribute, rather an input variable; eps no longer appears in the parent Simulator class

--- data_factory/simulators.py
+++ data_factory/simulators.py
@@ -20,39 +20,43 @@ class Simulator():
     self.dataset = SARSDataset(self.Nx, self.Ny)
     return self
 
-  def simulate(self, N, eps):
+  def simulate(self, N):
     for _ in range(N):
-      self.simulate_once(eps)
+      self.simulate_once()
     return self
-  
-  def get_dataloader(self, batch_size = 32):
-    return torch.utils.data.DataLoader(self.dataset, batch_size = batch_size, shuffle = True, drop_last = True)
 
-
-class QSimulator(Simulator):
-  def __init__(self, Nx, Ny, Q):
-    super().__init__(Nx, Ny)
-    self.Q = Q
-
-  def simulate_once(self, eps):
+  def simulate_once(self):
     game = Game(self.Nx, self.Ny)
     while game.t < self.T and not game.terminal():
       state = game.get_state()
-      action = self.eval_eps_greedy_action(state, eps)
+      action = self.sample_action(state)
       state_prime, reward = game.advance(action)
       term = torch.tensor(int(game.terminal())).type(torch.cuda.FloatTensor)
       t = torch.tensor(game.t).type(torch.cuda.FloatTensor)
       self.dataset.append(state, action, reward, state_prime, term, t)
     return self
+
+  def get_dataloader(self, batch_size = 32):
+    return torch.utils.data.DataLoader(self.dataset, batch_size = batch_size, shuffle = True, drop_last = True)
+
+
+class QSimulator(Simulator):
+  def __init__(self, Nx, Ny, Q, eps):
+    super().__init__(Nx, Ny)
+    self.Q = Q
+    self.eps = eps
+
+  def sample_action(self, state):
+    return self.eval_eps_greedy_action(state)
   
-  def eval_eps_greedy_action(self, state, eps):
+  def eval_eps_greedy_action(self, state):
     self.Q.eval()
     e = np.random.uniform()
-    if e > eps:
+    if e > self.eps:
       max_action = self.eval_max_action(state)
       return max_action
     else:
-      explore_action = np.random.randint(9)
+      explore_action = np.random.randint(Game.num_actions)
       return explore_action
 
   def eval_max_action(self, state):
