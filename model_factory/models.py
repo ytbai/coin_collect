8d9bc95f43127cfb777d4a13fbbe118960bdc480
Yuntao Bai <ytbai@princeton.edu>
2020-08-19 07:20:25 -0700

    Actor-critic baseline model

--- model_factory/models.py
+++ model_factory/models.py
@@ -112,3 +112,46 @@ class QValueDeep(nn.Module):
 
   def forward(self, x):
     return self.seq(x)
+
+
+class ACBaseline(nn.Module):
+  def __init__(self):
+    super().__init__()
+    
+    self.trunk = nn.Sequential(
+        nn.Conv2d(Game.num_channels, 8, kernel_size = 3, padding = 1),
+        nn.ReLU(),
+
+        ResidualBlock(8),
+        ResidualDownsample(8),
+        ResidualBlock(16),
+
+        GlobalAvgPool2d(),
+        nn.BatchNorm1d(16),
+        nn.ReLU(),
+        )
+
+    self.actor_head = nn.Sequential(
+        nn.Linear(16, Game.num_actions),
+        Rescale(1/np.sqrt(16)),
+        nn.Softmax(dim = 1),
+        )
+    
+    self.critic_head = nn.Sequential(
+        nn.Linear(16, 1),
+        Rescale(1/np.sqrt(16)),
+    )
+
+  def forward(self, x):
+    base = self.trunk(x)
+    actor_output = self.actor_head(base)
+    critic_output = self.critic_head(base)
+    return actor_output, critic_output
+
+  def actor(self, x):
+    base = self.trunk(x)
+    return self.actor_head(base)
+  
+  def critic(self, x):
+    base = self.trunk(x)
+    return self.critic_head(base).view(-1)
