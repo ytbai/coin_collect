7b11898a477b17ba66e73c51ca0741ea4a92ecb8
Yuntao Bai <ytbai@princeton.edu>
2020-08-20 13:45:06 -0700

    import ac_models

--- model_factory/__init__.py
+++ model_factory/__init__.py
@@ -2,3 +2,4 @@ from model_factory.modules import *
 from model_factory.model_factory import *
 from model_factory.q_models import *
 from model_factory.ac_models import *
+from model_factory.aq_models import *
